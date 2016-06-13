48c9ced41393a314a89cfc59c1a3a805543a40c4
e29hz <ehongzhen@qq.com>
2016-06-13 11:32:30 +0800

    增加 pch文件

--- YueShouApp/Hot/Controller/YSHotViewController.m
+++ YueShouApp/Hot/Controller/YSHotViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = YSRandomColor;
     self.navigationItem.title = @"热门";
 }
 

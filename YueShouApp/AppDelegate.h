1c5728824452e530a52aa8f145877e0d9b68aa71
e29hz <ehongzhen@qq.com>
2016-06-13 10:54:31 +0800

    Initial Commit

---
+++ YueShouApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YueShouApp
+//
+//  Created by 鄂鸿桢 on 16/6/13.
+//  Copyright © 2016年 xiaoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YueShouApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YueShouApp
+//
+//  Created by 鄂鸿桢 on 16/6/13.
+//  Copyright © 2016年 xiaoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
